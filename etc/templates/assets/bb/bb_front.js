04b2293d1a91ad307cd5034d9c8204f5d3f8cf6a
Fernando Guillen <fguillen.mail@gmail.com>
2012-12-01 19:53:27 +0100

    extras

--- etc/templates/assets/bb/bb_front.js
+++ etc/templates/assets/bb/bb_front.js
@@ -1,7 +1,20 @@
-$(window).load(function() {
+$(function() {
   $('.nivo_slider_performance').nivoSlider({
     effect: "fade",
     manualAdvance: true,
     animSpeed: 300
   });
+
+  extras_links();
 });
+
+
+function extras_links(){
+  $("#extras_elements ul li").each( function(index, element){
+    console.log( "element", element );
+    li_element = $(element);
+    img_element = $(element).find("img");
+
+    li_element.append("<div class=\"extra_title\">" + img_element.attr("title") + "</div>");
+  });
+}
