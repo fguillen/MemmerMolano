0e2c409e5458f09ff5aed9e269ca89099e52be5c
Fernando Guillen <fguillen.mail@gmail.com>
2013-02-22 21:02:51 +0100

    Preparing capistrano

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,7 +1,7 @@
-if Rails.env == "production"
+if Rails.env.production?
   BedAndBreakfast::Application.config.middleware.use(
     ExceptionNotifier,
-    :email_prefix => "[BandB] [Exception] ",
+    :email_prefix => "[MnM] [Exception] ",
     :sender_address => "fguillen.mail@gmail.com",
     :exception_recipients => "fguillen.mail@gmail.com"
   )
